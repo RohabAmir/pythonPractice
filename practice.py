c4454f15efae6131cf04cad7277f8f386c8279fe
RohabAmir <rohabamir2@gmail.com>
2022-10-24 11:45:25 +0500

    init commit

---
+++ practice.py
@@ -0,0 +1,136 @@
+# n=4
+# for i in range(4,-1,-1):
+#     print("*" * (i))
+
+
+# WITHOUT IF STATEMENT 
+# n=4
+# for i in range(4,0,-1):
+#     print("*" * (i))
+
+#WITH IF STATEMENT ( for a space in the end)
+# n=4
+# for i in range(5):
+#     if (i!=4):
+#         print("*" * (n-i))
+#     else :
+#         print(' ')
+
+
+
+
+# for i in range(0,7):
+#     if(i==3):
+#         print()  
+#         continue
+#     print("*****",end="")
+#     print()       
+
+
+
+
+
+# for i in range(0,4):
+#     print("*" * ( i+1))
+# for i in range(4,0,-1):
+#     print("*" * i)
+
+# n=4
+# for i in range (4):
+#     print(" " * (n-i-1), end="  ")
+#     print("*" * (2*i+2))
+# print()
+# for i in range (4,0,-1):
+#     print(" " * (n-i), end="  ")
+#     print("*" * (2*i))
+
+# n=4
+# for i in range (4):
+#     print(" " * (n-i-1), end="")
+#     print("*" * (i+1),end="")
+#     print(' ', end="")
+#     print("*" * (i+1))
+    
+# print()
+
+
+# for i in range (4,0,-1):
+#     print(" " * (n-i), end="")
+#     print("*" * i , end="")
+#     print(" ", end="")
+#     print("*" * i )
+
+######################################################################################################
+ 
+# def table(num):
+#     for i in range(1,11):
+#         print(f"{num}X{i}={num*i}")
+# num=int(input("Enter a number "))
+# table(num)
+
+
+# def factorial_iter(n):
+#     product=1
+#     for i in range(n):
+#         product= product*(i+1)
+#     return product
+# n=int(input("Enter a number "))
+# print(factorial_iter(n))
+
+# def factorial_rec(n):
+#     if (n==1 or n==0):
+#         return 1
+#     else:
+#         return n* factorial_rec(n-1)
+# n=int(input("Enter a number "))
+# print(factorial_rec(n))
+
+
+# def Sum_rec(n):
+#     if ( n==1 or n==0): # Base condition
+#         return 1
+#     else:
+#         return n + Sum_rec(n-1)
+# s=Sum_rec(5)
+# print(s)
+
+# name= "Rohab"                           
+# def getCapitalLetter():
+#     return name[0] #Block of code
+# name="Ahmed" #Variable Hoisting         name="Ahmed"
+
+
+
+
+
+# name= "Rohab"
+# def getCapitalLetter(): #scope of a function
+#     return name[0]
+# print(getCapitalLetter())
+# name="Ahmed" #Variable Hoisting
+# print(getCapitalLetter())
+
+# def greatest(n1,n2,n3):
+#     if (n1>n2):
+#         if(n1>n3):
+#             return n1
+#         else:
+#             return n3
+#     else: 
+#         if (n2>n3):
+#             return n2
+#         else:
+#             return n3
+
+# n1=int(input("Enter a number " ))
+# n2=int(input("Enter a number "))          
+# n3=int(input("Enter a number "))          
+# print(greatest(n1,n2,n3))
+
+
+
+
+
+
+
+
---
+++ practice_2.py
@@ -0,0 +1,40 @@
+# n=-10
+# a=-1
+# if (n > 5):
+#     n=50
+#     if ( n > 6):
+#         print(n)
+    
+#     if (n > 7):
+#         if(n>9):
+#             print("Rohab")
+# else :
+#     n=-60
+#     print(n)
+# n=3
+# def getN1(): #Difference between print and return
+#     print("Rohab")
+
+# def getN():
+#     return "Rohab" 
+
+# a = getN()
+# # a=getN1()
+# print(a)
+
+# function to print a pattern
+
+
+n=4
+def getN():
+    return n
+for i in range(getN()):
+        print('*' * (getN()-i))
+    
+
+
+
+
+
+
+
