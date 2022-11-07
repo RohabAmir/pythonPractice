5e764728aca07814f59831094404ac9385dd18ba
RohabAmir <rohabamir2@gmail.com>
2022-11-07 14:53:41 +0500

    pattern practice

---
+++ practice3.py
@@ -0,0 +1,8 @@
+n=3
+for i in range(0,n):
+    if(i==1):
+        print("*",end="")
+        print(" ",end="")
+        print("*")
+    else:
+        print("*" * n)
--- practice_2.py
+++ practice_2.py
@@ -1,16 +1,15 @@
-# n=-10
-# a=-1
-# if (n > 5):
-#     n=50
-#     if ( n > 6):
-#         print(n)
+n=-10
+a=-1
+if (n > 5):
+    n=50
+    if ( n > 6):
+        print(n)
     
-#     if (n > 7):
-#         if(n>9):
-#             print("Rohab")
-# else :
-#     n=-60
-#     print(n)
+    if (n > 7):
+        if(n>9):
+            print("Rohab")
+else :
+    print(n)
 # n=3
 # def getN1(): #Difference between print and return
 #     print("Rohab")
@@ -44,19 +43,19 @@
 # print(multiply(2,3,4,5,6,7,8,9))
 
 
-def saveUser(**user): # for dictionary to be a part of arguments 
-    print(user)
-saveUser(Rollno= 4492, Name="Rohab", age=23)
+# def saveUser(**user): # for dictionary to be a part of arguments 
+#     print(user)
+# saveUser(Rollno= 4492, Name="Rohab", age=23)
 
-def saveUser(**user): 
-    print(user["Name"])
-    print(user["Rollno"])
-    print(user["age"])
-
-
+# def saveUser(**user): 
+#     print(user["Name"])
+#     print(user["Rollno"])
+#     print(user["age"])
 
 
 
-saveUser(Rollno= 4492, Name="Rohab", age=23)
+
+
+# saveUser(Rollno= 4492, Name="Rohab", age=23)
 
 
