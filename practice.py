54cce7dc426f7ea1b7ae772906b691e1030a4e96
RohabAmir <rohabamir2@gmail.com>
2022-11-07 15:21:24 +0500

    Functions & Patterns

--- practice.py
+++ practice.py
@@ -4,61 +4,72 @@
 
 
 # WITHOUT IF STATEMENT 
-# n=4
-# for i in range(4,0,-1):
-#     print("*" * (i))
+n=4
+for i in range(4,0,-1):
+    print("*" * (i))
 
-#WITH IF STATEMENT ( for a space in the end)
-# n=4
-# for i in range(5):
-#     if (i!=4):
-#         print("*" * (n-i))
-#     else :
-#         print(' ')
-
-
-
-
-# for i in range(0,7):
-#     if(i==3):
-#         print()  
-#         continue
-#     print("*****",end="")
-#     print()       
+# WITH IF STATEMENT ( for a space in the end)
+n=4
+for i in range(5):
+    if (i!=4):
+        print("*" * (n-i))
+    else :
+        print(' ')
 
 
 
 
+for i in range(0,7):
+    if(i==3):
+        print()  
+        continue
+    print("*****",end="")
+    print()       
 
-# for i in range(0,4):
-#     print("*" * ( i+1))
-# for i in range(4,0,-1):
-#     print("*" * i)
 
-# n=4
-# for i in range (4):
-#     print(" " * (n-i-1), end="  ")
-#     print("*" * (2*i+2))
-# print()
-# for i in range (4,0,-1):
-#     print(" " * (n-i), end="  ")
-#     print("*" * (2*i))
 
-# n=4
-# for i in range (4):
-#     print(" " * (n-i-1), end="")
-#     print("*" * (i+1),end="")
-#     print(' ', end="")
-#     print("*" * (i+1))
+
+
+for i in range(0,4):
+    print("*" * ( i+1))
+
+for i in range(4,0,-1):
+    print("*" * i)
+
+
+n=4
+for i in range (4):
+    print(" " * (n-i-1), end="  ")
+    print("*" * (2*i+2))
+for i in range (4,0,-1):
+    print(" " * (n-i), end="  ")
+    print("*" * (2*i))
+
+n=4
+for i in range (4):
+    print(" " * (n-i-1), end="  ")
+    print("*" * (2*i+2))
+print()
+for i in range (4,0,-1):
+    print(" " * (n-i), end="  ")
+    print("*" * (2*i))
+
+
+n=4
+for i in range (4):
+    print(" " * (n-i-1), end="")
+    print("*" * (i+1),end="")
+    print(' ', end="")
+    print("*" * (i+1))
     
-# print()
+print()
 
 
-# for i in range (4,0,-1):
-#     print(" " * (n-i), end="")
-#     print("*" * i , end="")
-#     print(" ", end="")
-#     print("*" * i )
+for i in range (4,0,-1):
+    print(" " * (n-i), end="")
+    print("*" * i , end="")
+    print(" ", end="")
+    print("*" * i )
 
 ######################################################################################################
  
